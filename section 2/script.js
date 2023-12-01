f25b209e7ca624b0b3fdb4075ce3591599e9725d
shivanshu mishra <119070306+sxivansh@users.noreply.github.com>
2023-12-01 21:33:27 +0530

    Update script.js

--- section 2/script.js
+++ section 2/script.js
@@ -1,18 +1,29 @@
-const carousel = document.querySelector('.carousel');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const prevButton = document.getElementById('prevButton');
-const movebutton=Array.from(document.getElementsByClassName('section-items'));
-console.log(prevButton)
-const nextButton = document.getElementById('nextButton');
-let currentIndex = 0;
-movebutton.forEach((element,idx)=> {
-    element.addEventListener('click',()=>{
-        currentIndex=idx;
-        updateCarousel();
-    })
-});
+document.addEventListener("DOMContentLoaded", function () {
+   
+  
+    const carousel = document.querySelector(".carousel");
+    const carouselItems = document.querySelectorAll(".carousel-item");
+  
+    let currentIndex = 0;
+  
+    function showNextItem() {
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      updateCarousel();
+    }
+  
 
-function updateCarousel() {
-    const offset = -currentIndex * 500; 
-    carousel.style.transform = `translateX(${offset}px)`;
-}
+    function updateCarousel() {
+      const newTransformValue = -currentIndex * 100 + "%";
+      carousel.style.transform = `translateX(${newTransformValue})`;
+    }
+  
+    
+    function startAutomaticTransition() {
+      setInterval(showNextItem, 3000); 
+    }
+  
+ 
+    setTimeout(startAutomaticTransition, 2000);
+  });
+
+
